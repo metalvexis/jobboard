6eabe770c85b0165f990fb8be5fd8ea5f44a34b4
James Saballegue <jp.saballegue@gmail.com>
2024-02-07 10:07:25 +0800

    chore: setup local supabase

--- server/api/jobs/index.post.ts
+++ server/api/jobs/index.post.ts
@@ -1,8 +1,13 @@
+import { serverSupabaseServiceRole } from "#supabase/server";
 import { zh } from "h3-zod";
 import { zWorkzag } from "~/utils/zods";
+
 export default eventHandler(async (event) => {
+  const sbclient = serverSupabaseServiceRole(event);
+
   readRawBody(event);
 
   zh.useValidatedBody(event, zWorkzag);
+
   return `Jobs API handler`;
 });
